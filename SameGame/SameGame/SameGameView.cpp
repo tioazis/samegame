a8227e8cda1dc88a3e7437f0eee889a7aa9fbad2
Azis Jabbar Susetio <tioazis@gmail.com>
2018-07-27 23:22:46 +0700

    first commit

---
+++ SameGame/SameGame/SameGameView.cpp
@@ -0,0 +1,81 @@
+
+// SameGameView.cpp : implementation of the CSameGameView class
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "SameGame.h"
+#endif
+
+#include "SameGameDoc.h"
+#include "SameGameView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CSameGameView
+
+IMPLEMENT_DYNCREATE(CSameGameView, CView)
+
+BEGIN_MESSAGE_MAP(CSameGameView, CView)
+END_MESSAGE_MAP()
+
+// CSameGameView construction/destruction
+
+CSameGameView::CSameGameView()
+{
+	// TODO: add construction code here
+
+}
+
+CSameGameView::~CSameGameView()
+{
+}
+
+BOOL CSameGameView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: Modify the Window class or styles here by modifying
+	//  the CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CSameGameView drawing
+
+void CSameGameView::OnDraw(CDC* /*pDC*/)
+{
+	CSameGameDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: add draw code for native data here
+}
+
+
+// CSameGameView diagnostics
+
+#ifdef _DEBUG
+void CSameGameView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CSameGameView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CSameGameDoc* CSameGameView::GetDocument() const // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CSameGameDoc)));
+	return (CSameGameDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CSameGameView message handlers
---
+++ SameGame/SameGame/SameGameView.h
@@ -0,0 +1,46 @@
+
+// SameGameView.h : interface of the CSameGameView class
+//
+
+#pragma once
+
+
+class CSameGameView : public CView
+{
+protected: // create from serialization only
+	CSameGameView();
+	DECLARE_DYNCREATE(CSameGameView)
+
+// Attributes
+public:
+	CSameGameDoc* GetDocument() const;
+
+// Operations
+public:
+
+// Overrides
+public:
+	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// Implementation
+public:
+	virtual ~CSameGameView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // debug version in SameGameView.cpp
+inline CSameGameDoc* CSameGameView::GetDocument() const
+   { return reinterpret_cast<CSameGameDoc*>(m_pDocument); }
+#endif
+
